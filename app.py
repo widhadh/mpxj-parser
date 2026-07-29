506d1a47d0621e9d7dbdf145a9319187448ab120
widhadh <widhadhw@me.com>
2026-07-29 11:58:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,10 @@ jpype.startJVM()
 from org.mpxj.reader import UniversalProjectReader
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 # ── Link type mapping ─────────────────────────────────────────────────────────
